4fe61a5af9ec1aae14485df851492cb13cf7249c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-13 14:25:34 +0300

    Add ruff+black

--- tests/test_native.py
+++ tests/test_native.py
@@ -8,9 +8,7 @@ import lzokay
     b"Hello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello Worldello World",
 ])
 def test_compress_and_decompress(data):
-
     compressed = lzokay.compress(data)
-    print(compressed)
 
     decompressed = lzokay.decompress(compressed)
 
