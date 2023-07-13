062dc271a3fdca51ba17eeaaf151acc3d51df4b5
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-13 13:12:09 +0300

    Moving code to src

--- setup.py
+++ setup.py
@@ -154,9 +154,6 @@ for ext_module in cythonized_ext_modules:
 
 
 setup(
-    use_scm_version={
-        "local_scheme": "no-local-version",
-    },
     cmdclass={
         'build_ext': CMakeBuild,
     },
